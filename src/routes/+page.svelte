0d30b20c6a30a4a67fffb9da7b0d3d71ab8d03aa
Logan <logan@fireflysoftware.dev>
2023-11-30 12:38:40 -0500

    Installed melt-ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1 class="text-3xl font-bold text-green-600">Welcome to SvelteKit</h1>
+<script>
+  import { createCollapsible, melt } from '@melt-ui/svelte'
+  const {
+    elements: { root, content, trigger },
+    states: { open }
+  } = createCollapsible()
+</script>
+
+ <h1 class="text-3xl font-bold text-green-600">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div use:melt={$root}>
+  <button use:melt={$trigger}>{$open ? 'Close' : 'Open'}</button>
+  <div use:melt={$content}>Obi-Wan says: Hello there!</div>
+</div>
