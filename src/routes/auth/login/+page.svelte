e0e622df1ff145797c4e1260db70fe845628d353
Logan <logan@fireflysoftware.dev>
2023-11-30 15:50:35 -0500

    Dark mode implemented by extendign colors rather than overwriting them in the tailwind.config.js

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,4 +1,4 @@
-<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+<div class="dark:bg-gray-900 flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
 	<div class="sm:mx-auto sm:w-full sm:max-w-sm">
 		<img
 			class="mx-auto h-10 w-auto"
