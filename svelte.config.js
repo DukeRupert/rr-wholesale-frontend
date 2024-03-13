fe0278b4ee6c3abc988bfc3f5c0ef450b7cf3bec
Logan Williams <logan@fireflysoftware.dev>
2024-03-13 18:17:31 +0000

    Swithced to adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config}*/
@@ -8,7 +8,13 @@ const config = {
 	// for more information about preprocessors
 	preprocess: sequence([vitePreprocess({}), preprocessMeltUI()]),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			images: {
+				sizes: [320, 640, 768, 1024, 1280, 1920, 2560],
+				formats: ['image/avif', 'image/webp', "image/jpeg"],
+				minimumCacheTTL: 300
+			}
+		}),
 		csrf: {
 			checkOrigin: false
 		}
