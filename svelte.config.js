893dfc9039339a474d3974e00266cc5cf2fe64b5
Logan Williams <logan@fireflysoftware.dev>
2024-03-13 17:38:34 +0000

    Reverted to adapter-auto due to build issue with adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,14 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: sequence([vitePreprocess({}), preprocessMeltUI()]),
 	kit: {
-		adapter: adapter({
-			images: {
-				sizes: [320, 640, 768, 1024, 1280, 1920, 2560, 3840],
-				formats: ['image/avif', 'image/webp', 'image/jpeg'],
-				minimumCacheTTL: 300
-			}
-		}),
+		adapter: adapter(),
 		csrf: {
 			checkOrigin: false
 		}
